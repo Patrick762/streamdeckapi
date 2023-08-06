1cc3f4ae3b2bfc59386408572430728793faa166
Patrick762 <lutzpatti86@gmail.com>
2023-08-06 16:42:09 +0200

    set pillow version to latest

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         "human-readable-ids==0.1.3",
         "jsonpickle==3.0.1",
         "streamdeck==0.9.3",
-        "pillow>=9.4.0,<10.0.0",
+        "pillow",
         "cairosvg==2.7.0",
         "zeroconf",
     ],
